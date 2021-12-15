7c531fac2a9218c8675f0a8e995ccf818d2a58b5
reagankporter <reagankporter@gmail.com>
2021-12-14 22:57:20 -0500

    url change

--- src/helpers/enviroment.js
+++ src/helpers/enviroment.js
@@ -4,7 +4,7 @@ switch(window.location.hostname) {
     case "localhost" || "127.0.0.1":
         APIURL = "http://localhost:3001"
         break;
-    case "selfqclient.herokuapp.com":
+    case "selfqclienttsxtwo.herokuapp.com":
         APIURL = "https://selfqserver.herokuapp.com"
         break;
     default:
